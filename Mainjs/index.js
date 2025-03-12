c41a6072042dabdc4225d3ee5e4add432c88d5e1
mahmoudNEWdom <pprokiller2@gmail.com>
2025-03-12 09:13:01 +0200

    ddd

--- Mainjs/index.js
+++ Mainjs/index.js
@@ -42,7 +42,7 @@ login.addEventListener('click', function() {
     }
     if (NameRegex.test(Name.value) && EmailRegex.test(Email.value) && PasswordRegex.test(Password.value) && (Confirm.value == Password.value)) {
         savecookie('Myuser',Name.value);
-        window.location.href = "../Mainhtml/page2.html";
+        window.location.href = "/Mainhtml/page2.html";
     }
 });
 /* ----------------cookies------------------ */
